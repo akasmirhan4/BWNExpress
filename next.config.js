691ef6fbb070bdbeefca334f12cf1ef43b0dd2cb
akasmirhan4 <amirrul.kasmirhan@gmail.com>
2021-11-26 21:57:01 +0800

    changed next config for image opt

--- next.config.js
+++ next.config.js
@@ -1,11 +1,3 @@
 module.exports = {
 	reactStrictMode: true,
-	env: {
-		GOOGLE_ID: "173660637847-mmjhlif6k240om0auc17ivpfhdbhadsu.apps.googleusercontent.com",
-		GOOGLE_SECRET: "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",
-	},
-	images: {
-		loader: "imgix",
-		path: "https://noop/",
-	},
 };
