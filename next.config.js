4762877b4ab9f1a2e158f2e0dfa085f5f1653f94
akasmirhan4 <amirrul.kasmirhan@gmail.com>
2021-11-26 20:55:07 +0800

    important shadows

--- next.config.js
+++ next.config.js
@@ -4,4 +4,8 @@ module.exports = {
 		GOOGLE_ID: "173660637847-mmjhlif6k240om0auc17ivpfhdbhadsu.apps.googleusercontent.com",
 		GOOGLE_SECRET: "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",
 	},
+	images: {
+		loader: "imgix",
+		path: "https://noop/",
+	},
 };
